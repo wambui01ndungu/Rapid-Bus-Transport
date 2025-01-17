827c3ac72e7339b824c9bf07d1d61fbfc5504bf1
Shukri Abdi <shukri751@gmail.com>
2025-01-17 20:37:08 +0300

    Initial deployment

--- server/app.py
+++ server/app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+import os
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Welcome to the bus booking Api"
+app = Flask(__name__, static_folder="build")
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, "index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=False)
