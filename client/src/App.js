ea478cb455c59e838a63fe2d0adc525b87ed014a
innocent <iparainnocent@gmail.com>
2025-01-22 23:16:59 +0300

    added drivermanagement.js file and worked on it as required

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
-import {useEffect} from "react"
+import React from "react";
+import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
     document.title = "Rapid Bus Transport";
-  },[]);
-  
-  return (
-    <div>
-      { <h1> Welcome To Rapid Bus Transport</h1>}
-    </div>
-  );
+  }, []);
+
+  return <div>{<h1> Welcome To Rapid Bus Transport</h1>}</div>;
 }
-   
 
 export default App;
