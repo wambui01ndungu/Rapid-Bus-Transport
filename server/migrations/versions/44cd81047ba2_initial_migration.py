fe138f3f94f67d0c75ba647a5700c972ca427691
Wambui Ndungu <wambuindungu4967@gmail.com>
2025-02-08 05:14:45 +0400

    adds migrations

--- server/migrations/versions/44cd81047ba2_initial_migration.py
+++ server/migrations/versions/44cd81047ba2_initial_migration.py
@@ -1,8 +1,8 @@
-"""initial migration
+"""Initial migration
 
-Revision ID: eb486015a000
+Revision ID: 44cd81047ba2
 Revises: 
-Create Date: 2025-02-03 19:01:05.721795
+Create Date: 2025-02-08 05:04:04.900604
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'eb486015a000'
+revision = '44cd81047ba2'
 down_revision = None
 branch_labels = None
 depends_on = None
